25254b8e7fd144dcda85decdb4a9b2d2504347f1
wattnotions <shaneormonde@gmail.com>
2020-09-20 13:07:31 +0100

    rpc receive almost fully working

---
+++ Arduino_PLC/arduino_programs/mqtt_receive_rpc/mqtt_receive_rpc.ino
@@ -0,0 +1,103 @@
+/*
+ Basic MQTT example
+
+ This sketch demonstrates the basic capabilities of the library.
+ It connects to an MQTT server then:
+  - publishes "hello world" to the topic "outTopic"
+  - subscribes to the topic "inTopic", printing out any messages
+    it receives. NB - it assumes the received payloads are strings not binary
+
+ It will reconnect to the server if the connection is lost using a blocking
+ reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+ achieve the same result without blocking the main loop.
+ 
+*/
+
+#include <SPI.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
+
+byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
+IPAddress ip(192, 168, 0, 45);
+IPAddress server(64, 227, 46, 82);
+
+//const char* server = "test.mosquitto.org";
+
+void callback(char* topic, byte* payload, unsigned int length, PubSubClient *client) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  String RPC_num = getValue(topic,'/',5);
+  Serial.print("] ");
+
+
+  &client.publish("v1/devices/me/rpc/response/" + RPC_num, "");
+
+  
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+EthernetClient ethClient;
+PubSubClient client(ethClient);
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("Arduino_mqtt", "RASPBERRY_PI_DEMO_TOKEN", "")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      //client.publish("v1/devices/me/telemetry", "{\"temperature\": 45, \"humidity\": 88}");
+      // ... and resubscribe
+      client.subscribe("v1/devices/me/rpc/request/+");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");// Update these with values suitable for your network.
+
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup()
+{
+  Serial.begin(57600);
+
+  client.setServer(server, 1883);
+  client.setCallback(callback);
+
+  Ethernet.begin(mac, ip);
+  // Allow the hardware to sort itself out
+  delay(1500);
+}
+
+void loop()
+{
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
+
+
+String getValue(String data, char separator, int index)
+{
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length()-1;
+
+  for(int i=0; i<=maxIndex && found<=index; i++){
+    if(data.charAt(i)==separator || i==maxIndex){
+        found++;
+        strIndex[0] = strIndex[1]+1;
+        strIndex[1] = (i == maxIndex) ? i+1 : i;
+    }
+  }
+
+  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
