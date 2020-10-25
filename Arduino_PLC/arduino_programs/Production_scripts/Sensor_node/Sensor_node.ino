c1ffdc286d4e445f1a50a8c543999f6bb07de251
wattnotions <shaneormonde@gmail.com>
2020-10-25 09:45:05 +0000

    added c02 code and extra comments

--- Arduino_PLC/arduino_programs/Production_scripts/Sensor_node/Sensor_node.ino
+++ Arduino_PLC/arduino_programs/Production_scripts/Sensor_node/Sensor_node.ino
@@ -34,15 +34,16 @@ unsigned long delayTime;
 // data array for modbus network sharing
 uint16_t ModbusSReg[8] = {1112,232,344,4,5,6,7,8};   //modbus slave registers
 
+//senseair s8 defs
+SoftwareSerial K_30_Serial(4,5);   
+
+byte readCO2[] = {0xFE, 0X44, 0X00, 0X08, 0X02, 0X9F, 0X25};  //Command packet to read Co2 (see app note)
+byte response[] = {0,0,0,0,0,0,0};  //create an array to store the response
+
+//multiplier for value. default is 1. set to 3 for K-30 3% and 10 for K-33 ICB
+int valMultiplier = 1;
 
 
-/**
- *  Modbus object declaration
- *  u8id : node id = 0 for master, = 1..247 for slave
- *  port : serial port
- *  u8txenpin : 0 for RS-232 and USB-FTDI 
- *               or any pin number > 1 for RS-485
- */
 Modbus slave(1,mySerial,TXEN); // this is slave @1 and RS-485
 
 void setup() {
@@ -56,6 +57,9 @@ void setup() {
   //set up bme280
   unsigned status;
   status = bme.begin(0x76);  
+
+  //set up s8 serial
+  K_30_Serial.begin(9600); 
   
   if (!status) {
       Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
@@ -73,11 +77,16 @@ void setup() {
 int num_bytes=0;
 void loop() {
   slave.poll( ModbusSReg, 8 );
-  printValues();
+  getSensorValues();
+
+  
 
 }
 
-void printValues() {
+
+//gets sensor values and puts them into modbus array
+//this array is then polled and sent via modbus
+void getSensorValues() {
 
     static int temp, pres, hum;
     Serial.print("Temperature = ");
@@ -98,5 +107,48 @@ void printValues() {
     Serial.print(hum);
     Serial.println(" %");
 
+    sendRequest(readCO2);
+    unsigned long valCO2 = getValue(response);
+    ModbusSReg[3] = hum;
+    Serial.println(valCO2);
+
     Serial.println();
+}
+
+void sendRequest(byte packet[])
+{
+  while(!K_30_Serial.available())  //keep sending request until we start to get a response
+  {
+    K_30_Serial.write(readCO2,7);
+    delay(50);
+  }
+  
+  int timeout=0;  //set a timeoute counter
+  while(K_30_Serial.available() < 7 ) //Wait to get a 7 byte response
+  {
+    timeout++;  
+    if(timeout > 10)    //if it takes to long there was probably an error
+      {
+        while(K_30_Serial.available())  //flush whatever we have
+          K_30_Serial.read();
+          
+          break;                        //exit and try again
+      }
+      delay(50);
+  }
+  
+  for (int i=0; i < 7; i++)
+  {
+    response[i] = K_30_Serial.read();
+  }  
+}
+
+unsigned long getValue(byte packet[])
+{
+    int high = packet[3];                        //high byte for value is 4th byte in packet in the packet
+    int low = packet[4];                         //low byte for value is 5th byte in the packet
+
+  
+    unsigned long val = high*256 + low;                //Combine high byte and low byte with this formula to get value
+    return val* valMultiplier;
 }
