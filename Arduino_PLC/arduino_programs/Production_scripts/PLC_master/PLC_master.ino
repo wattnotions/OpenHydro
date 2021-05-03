9b100a311d37dbbcf4313e83583520db05407644
wattnotions <shane.ormonde@eiratech.com>
2021-05-03 16:39:58 +0100

    basic on off control implemented - untested

--- Arduino_PLC/arduino_programs/Production_scripts/PLC_master/PLC_master.ino
+++ Arduino_PLC/arduino_programs/Production_scripts/PLC_master/PLC_master.ino
@@ -1,3 +1,4 @@
+
 #include <Controllino.h>  /* Usage of CONTROLLINO library allows you to use CONTROLLINO_xx aliases in your sketch. */
 #include "ModbusRtu.h"    /* Usage of ModBusRtu library allows you to implement the Modbus RTU protocol in your sketch. */
 #include <SPI.h>
@@ -52,13 +53,7 @@ float Temp_SP, InFan_SP, OutFan_SP;
 // variables to store latest modbus sensor values from first sensor node (Node A)
 float TempA, PresA, HumA;
 
-
-
-
-
-
-
-
+unsigned long int start_time=0;
 
 void setup() {
    // initialize serial communication at 9600 bits per second:
@@ -71,17 +66,17 @@ void setup() {
   ModbusQuery[0].u8id = SlaveModbusAdd; // slave address
   ModbusQuery[0].u8fct = 3; // function code (this one is registers read)
   ModbusQuery[0].u16RegAdd = 0; // start address in slave
-  ModbusQuery[0].u16CoilsNo = 4; // number of elements (coils or registers) to read
+  ModbusQuery[0].u16CoilsNo = 5; // number of elements (coils or registers) to read
   ModbusQuery[0].au16reg = ModbusSlaveRegisters; // pointer to a memory array in the CONTROLLINO
 
   // ModbusQuery 1: write a single register
   ModbusQuery[1].u8id = SlaveModbusAdd; // slave address
   ModbusQuery[1].u8fct = 6; // function code (this one is write a single register)
-  ModbusQuery[1].u16RegAdd = 4; // start address in slave
+  ModbusQuery[1].u16RegAdd = 6; // start address in slave
   ModbusQuery[1].u16CoilsNo = 1; // number of elements (coils or registers) to write
   ModbusQuery[1].au16reg = ModbusSlaveRegisters+4; // pointer to a memory array in the CONTROLLINO
   ModbusSlaveRegisters[4] = 1; // initial value for the relays 
-	
+  
   ControllinoModbusMaster.begin( 19200 ); // baud-rate at 19200
   ControllinoModbusMaster.setTimeOut( 5000 ); // if there is no answer in 5000 ms, roll over
   
@@ -91,6 +86,7 @@ void setup() {
 
   //mqtt / ethernet setup
   client.setServer(server, 1883);
+  
   client.setCallback(callback);
 
   Ethernet.begin(mac, ip);
@@ -103,14 +99,19 @@ void setup() {
   pinMode(CONTROLLINO_D11, OUTPUT);
   
   digitalWrite(CONTROLLINO_R0, HIGH);
+
+  start_time = millis();
 }
 
 //modbus values storage
 
 
-int i;
+int i,addr,change_addr;
+int slave_select = 1;
 
 char *tptr, *pptr, *hptr, *cptr;
+int tempOUT,presOUT,humOUT,co2OUT,tempIN,presIN,humIN,co2IN;
+
 
 void loop() {
 
@@ -118,10 +119,15 @@ void loop() {
       reconnect();
     }
     client.loop();
+
+
+    if (millis() - start_time > 60000){
+      start_time = millis();
+      on_off_control();
+      Serial.print("HELLO");
+    }
     
-  
-    ControllinoModbusMaster.query( ModbusQuery[0] ); // send query (only once) 0 is read 1 is write
-    ControllinoModbusMaster.poll(); // check incoming messages
+    
 
     //non blocking delay - sends mqtt every few seconds
     i++;
@@ -131,60 +137,82 @@ void loop() {
       for (int h=0; h<4; h++){
         mqtt_pub(h);
       }
+      Serial.print("SLAVE_ADDR = ");
+      Serial.println(ModbusSlaveRegisters[4]);
+
+      change_addr = !change_addr;
+      
+      if(change_addr==1){addr=1;} 
+      else{addr=7;}
+      set_slave_address(addr);
+      slave_select++;
+      if (slave_select > 2) slave_select=1;
+      
+      ControllinoModbusMaster.query( ModbusQuery[0] ); // send query (only once) 0 is read 1 is write
+      ControllinoModbusMaster.query( ModbusQuery[0] ); // send query (only once) 0 is read 1 is write
+      ControllinoModbusMaster.query( ModbusQuery[0] ); // send query (only once) 0 is read 1 is write
+       ControllinoModbusMaster.poll(); // check incoming messages
     }
     
-    
-  
-    
-
+    ControllinoModbusMaster.poll(); // check incoming messages
 
     
+}
 
+void on_off_control(){
+  if (tempIN < Temp_SP){
+    analogWrite(CONTROLLINO_D9, 30);
+    Serial.println("HEAT ON");
+  }
+  else{
+    analogWrite(CONTROLLINO_D9, 0);
+    Serial.println("HEAT OFF");
+  }
 }
 
 //this function gets a sensor reading from the the modbus slave register on this device (periodically updated)
 //it then formats the int value to a string and appends it to another string needed for mqtt/thingsboard
+
+
 void mqtt_pub(int memaddr){  // 0 temp, 1 pres, 2 humidity, 3 c02
   char str_temp[10];
   char tstr[10];
   char mqtt_body[100];
   float temp = float( (ModbusSlaveRegisters[memaddr]) );
+
+  if (ModbusSlaveRegisters[4]==7){  //this code is terrible   //slave with address 7
+    if (memaddr == 0) {strcpy(mqtt_body,"{\"temperatureOUT\": "); temp = temp /100; TempA = temp; tempOUT=temp;}
+    if (memaddr == 1) {strcpy(mqtt_body,"{\"pressureOUT\": "); PresA = temp; presOUT=temp;}
+    if (memaddr == 2) {strcpy(mqtt_body,"{\"humidityOUT\": "); temp = temp/100; HumA = temp; humOUT=temp;}
+    if (memaddr == 3) {strcpy(mqtt_body,"{\"c02_OUT\": "); co2OUT=temp;}
+  }
+
+   if (ModbusSlaveRegisters[4]==5){  //this code is terrible  //actual addr is 1 but 5 is in mem addr on that slave
+    if (memaddr == 0) {strcpy(mqtt_body,"{\"temperatureIN\": "); temp = temp /100; TempA = temp; tempIN=temp;}
+    if (memaddr == 1) {strcpy(mqtt_body,"{\"pressureIN\": "); PresA = temp; presIN=temp;}
+    if (memaddr == 2) {strcpy(mqtt_body,"{\"humidityIN\": "); temp = temp/100; HumA = temp; humIN=temp;}
+    if (memaddr == 3) {strcpy(mqtt_body,"{\"c02_IN\": "); co2IN=temp;}
+  }
+
   
-  if (memaddr == 0) {strcpy(mqtt_body,"{\"temperature\": "); temp = temp /100; TempA = temp;}
-  if (memaddr == 1) {strcpy(mqtt_body,"{\"pressure\": "); PresA = temp;}
-  if (memaddr == 2) {strcpy(mqtt_body,"{\"humidity\": "); temp = temp/100; HumA = temp;}
-  if (memaddr == 3) {strcpy(mqtt_body,"{\"c02\": ");}
 
   dtostrf(temp, 4, 2, str_temp);
   sprintf(tstr, "%s", str_temp);
   strcat(mqtt_body, tstr);
   tptr = strcat(mqtt_body, "}");
-  Serial.println(tptr);
+  //Serial.println(tptr);
+  
  
 
   client.publish("v1/devices/me/telemetry", tptr);
 
 }
 
-//print sensor values coming in over modbus for address 0-4
-//these registers are on this device but are periodically updated
-// needs modbus polling code etc to work
-void print_modbus_registers(){
-   // registers read was proceed
-          Serial.println("---------- READ RESPONSE RECEIVED ----");
-          Serial.print("Slave ");
-          Serial.print(SlaveModbusAdd, DEC); 
-          Serial.print(" Reg 0 : ");
-          Serial.print(ModbusSlaveRegisters[0],DEC);
-          Serial.print(" , Reg 1: ");
-          Serial.print(ModbusSlaveRegisters[1],DEC);
-          Serial.print(" , Reg2 : ");
-          Serial.print(ModbusSlaveRegisters[2], DEC);
-          Serial.print(" , Reg3 : ");
-          Serial.println(ModbusSlaveRegisters[3], DEC);
-          Serial.println("-------------------------------------");
-          Serial.println("");
-}
+
+
+
+
+
 
 
 
@@ -264,17 +292,18 @@ void thingsbUpdate(char* method, float param){
   
 }
 
-//update the local variables that hold the system setpoints
+//update the local variables that hold the system setpoints received from the thingsboard dashboard
 void updateSetpoints(float Temp_SP, float InFan_SP, float OutFan_SP){
   int tempVal = map(int(Temp_SP), 0, 100, 0, MAX_TEMP);
   if( (tempVal < MAX_TEMP) & (tempVal > MIN_TEMP) ) {
-    analogWrite(CONTROLLINO_D9, int(tempVal));
+    
+    Serial.print(Temp_SP);
     Serial.println("TEMP UPDATED");
   }
 
   int infanVal = map(int(InFan_SP), 0, 100, 0, 255);
   analogWrite(CONTROLLINO_D11, int(infanVal));
-  Serial.println(infanVal);
+  
 
   int outfanVal = map(int(OutFan_SP), 0, 100, 0, 255);
   analogWrite(CONTROLLINO_D10, int(outfanVal));
@@ -299,4 +328,9 @@ String getValue(String data, char separator, int index)
   }
 
   return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+void set_slave_address(int address){
+  ModbusQuery[0].u8id = address; // slave address for reads
+  ModbusQuery[1].u8id = address; // slave address for writes
 }
